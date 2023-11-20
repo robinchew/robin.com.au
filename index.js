dc3ecb49943532ca1f1f7ca39088523749cdc4a1
Andy Lam Yu <andyulam1999@gmail.com>
2023-11-19 19:29:08 -0800

    Committing the remaining code for the project

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+/**
+ * Takes in a list of elements and returns a list of virtual of DOM Elemnts
+ */
+function ConvertListToDOM(listOfElements) {
+    var ListOfDOM = [];
+
+    for(var i = 0; i < listOfElements.length; i++)
+    {
+        // Check if there is a nested/sub list by checking for an object instead of an array
+        if(typeof(listOfElements[i]) === 'object' && !Array.isArray(listOfElements[i])) {
+            if(Array.isArray(listOfElements[i].title)) {
+                listOfElements[i].title.push(['ul', listOfElements[i].list]);
+                ListOfDOM.push(ConvertToHTML(['li', listOfElements[i].title]));
+            } else {
+                ListOfDOM.push(ConvertToHTML(['li', [listOfElements[i].title, ['ul', listOfElements[i].list]]]));
+            }
+        // Checks if there is a Hyperlink as a item in the list
+        } else if(listOfElements[i][0] === 'a') {
+            ListOfDOM.push(ConvertToHTML(['li', ConvertToHTML(listOfElements[i])]));
+        } else {
+            ListOfDOM.push(ConvertToHTML(['li', listOfElements[i]]));
+        }
+    }
+
+    return m('ul', ListOfDOM);
+}
+
 function ConvertToHTML(element) {
     var domElement = m('div', element);
 
     if(Array.isArray(element)) {
         if(element.length == 2) {
-            domElement = ConvertToDOM(element[0], element[1]);
+            if(element[0] === 'ul') {
+                domElement = ConvertListToDOM(element[1]);
+            } else {
+                if(Array.isArray(element[1])) {
+                    domElement = m(element[0], element[1].map((element) => typeof(element) === 'object' ? ConvertToHTML(element) : element));
+                } else {
+                    domElement = m(element[0], element[1]);
+                }
+            }
         } else {
-            domElement = m(element[0], element[1], element[2]);
+            if(Array.isArray(element[2])){
+                domElement = m(element[0], element[1], element[2].map((element) => typeof(element) === 'object' ? ConvertToHTML(element) : element));
+            } else {
+                domElement = m(element[0], element[1], element[2]);
+            }
         }
-    }
-
-    return domElement;
-}
-
-function ConvertToDOM(tag, content) {
-    var domElement = m(tag, content);
-
-    if(Array.isArray(content)) {
-        domElement = m(tag, content.map((element) => typeof(element) === 'object' ? ConvertToHTML(element) : element));
     }
 
     return domElement;
--- resume.js
+++ resume.js
@@ -4,6 +4,210 @@ const expDesciption = [
     ") with immutable data and composable functions, rather than object-oriented code."
 ];
 
+const frontendStacks = [
+    'Javascript + Mithril/React'
+];
+
+const backendStacks = [
+    'Python + Flask/Django',
+    'Erlang + Cowboy',
+    'Clojure',
+    'ClojureScript'
+];
+const familiarStacks = [
+    'Elixir + Phoenix',
+    'Kotlin',
+    'Swift',
+    'Dart + Flutter'
+];
+const lessFamiliar = [
+    'C#/.NET Core',
+    'Java',
+    'G'
+];
+
+const skills = [
+    'Arch Linux, Ubuntu, Debian, Red Hat',
+    'Vim, Android Studio, Xcode',
+    'PostgreSQL, MySQL, MSSQL, SQLite',
+    'Git with Github, Gitlab, Bitbucket',
+    'Bash Scripting',
+    'HTML/CSS',
+    'Photoshop',
+    'LXC, Docker, VirtualBox/Vagrant, VMWare, etc.',
+    'AWS services: EC2, S3, Lambda, DynamoDB, Elastic Beanstalk, etc.',
+    {
+        title: [
+            'Co-organiser of the monthly Python meetup ',
+            ['a',{href:'http://pythonwa.com'},'http://pythonwa.com']
+        ],
+        list: [{
+            title: 'Talked about how not to use Django',
+            list: [
+                ['a',{href:'https://www.meetup.com/Perth-Django-Users-Group/events/237759934/'},'https://www.meetup.com/Perth-Django-Users-Group/events/237759934/']
+            ]
+        }, {
+            title: [
+                'Introduce Single Page Applications using ',
+                ['a',{href:'https://mithril.js.org'},'Mithril'],
+                ' instead of React.'
+            ],
+            list: [
+                ['a',{href:'https://www.meetup.com/Perth-Django-Users-Group/events/241878300/'},'https://www.meetup.com/Perth-Django-Users-Group/events/241878300/'],
+                ['a',{href:'https://www.meetup.com/Perth-Django-Users-Group/events/240861484/'},'https://www.meetup.com/Perth-Django-Users-Group/events/240861484/']
+            ]
+        }]
+    },
+    {
+        title: 'Teaching Python and Javascript to aspiring developers',
+        list: [
+            ['a',{href:'https://www.meetup.com/pythonwa/events/290315903/'},'https://www.meetup.com/pythonwa/events/290315903/']
+        ]
+    },
+    {
+        title: [
+            'As guest programming workshop presenter for Perth Machine Learning Group ',
+            ['a',{href:'https://www.pmlg.org/'},'pmlg.org']
+        ],
+        list: [{
+            title: 'Kick out Meetup spam bots automatically with Python',
+            list: [
+                ['a',{href:'https://www.meetup.com/Perth-Machine-Learning-Group/events/kppfhryzpbsb/'},'https://www.meetup.com/Perth-Machine-Learning-Group/events/kppfhryzpbsb/']
+            ]
+        }]
+    }
+];
+
+const contract2023 = [
+    'Continued work for Dinner Twist to analyse packing data.'
+];
+
+const contract2022 = [
+    'Got sick of programming and became a barista to run a cafe for a month. I don\'t even drink coffee.',
+    'Built a development team to do e-commerce integration for a salon supply company. (Python, Wordpress/Woocommerce/PHP, Erlang)',
+    'Social chat programme (Python, Erlang, JavaScript/WebSocket, PostgreSQL)',
+    [
+        'Semi-automatic food picking system to reduce worker pack time and eliminate human errors for ',
+        ['a',{href:'https://www.dinnertwist.com.au'},'https://www.dinnertwist.com.au'],
+        ' (Erlang, JavaScript/WebSocket)'
+    ]
+];
+
+const quotedWork2021 = [
+    'Built a development team to develop a platform for a business that connects service providers and staff. (Python, JavaScript, PostgreSQL, Dart + Flutter)'
+];
+
+const partTime2021 = [
+    'Software consulting for Woodside through a recruiter. (Python)',
+    [
+        'Software development for NFORM ',
+        ['a',{href:'https://nform.com.au/'},'https://nform.com.au/'],
+        ' (Python, JavaScript, HTML)'
+    ],
+    [
+        'Software consulting and staff training for Harlsan ',
+        ['a',{href:'https://www.harlsan.com.au/'},'https://www.harlsan.com.au/'],
+        ' (Git, Python, Tableau integration)'
+    ]
+];
+
+const contract2020 = [
+    'Full-time Software development contract for Rio Tinto through a recruiter. (Python, JavaScript, Vagrant, Jenkins, ...)'
+];
+
+const contract2019 = [
+    'Django development for a property rental and sales site',
+    [
+        'Python development and devops (Docker) for ',
+        ['a',{href:'https://threespringstechnology.com'},'https://threespringstechnology.com'],
+        ' that uses AI for medical imaging'
+    ],
+    [
+        'Elixir development for ',
+        ['a',{href:'https://tyreconnect.com.au'},'https://tyreconnect.com.au']
+    ],
+    'Sub-contracted Kotlin development for sailing-related mobile app',
+    'After hours assistant teaching Scratch for ICTLabs in Riverlands Montessori School'
+];
+
+const obsiProjDesc = [
+    "OBSI is a software consultancy company that I've started, and below are the list of projects that I've completed for clients. See some screenshots at "
+    ['a',{href:'http://obsi.com.au'}, 'http://obsi.com.au'],
+    " or else I am able to demonstrate these applications on request."
+];
+
+const obsiProject2019 = {
+    projName: 'POS/KDS (Point of Sale/Kitchen Display System)',
+    desc: 'Offline web application for displaying orders for restaurant kitchens pushed from POS.',
+    techStack: [
+        'UI (Javascript/Mithril)',
+        'API Server (Erlang/Cowboy)',
+        'SQL (No ORM)',
+        'Integration with EFTPOS terminals (C#, .NET Core)'
+    ]
+};
+
+const obsiProject2017 = {
+    projName: 'Teacher Search',
+    desc: 'A short-lived startup that connects schools and teachers.',
+    techStack: [
+        'Web Frontend: JavaScript',
+        'iOS: Swift',
+        'Android: Kotlin',
+        'Backend: Python'
+    ]
+};
+
+const obsiProject2016 = {
+    projName: 'Ciao Bella Nail Salon',
+    owner: 'Alfred Filser (0401662042)',
+    desc: "This beauty salon has 2 branches and staff are expected to move between those branches with clients who are willing to move between branches to access the service in high demand. The business owner uses OBSI's web application with a timetable interface in order to create and occasionally move client/staff bookings between branches in order to satisfy demand.",
+    techStack: [
+        'API Server (Python/Django)',
+        'UI (Clojurescript/Reagent, Javascript/React)',
+        'Custom CI server (Go)',
+        'Amazon Web Services'
+    ]
+};
+
+const obsiProject2015 = {
+    projName: 'Lightcube Designs',
+    owner: 'Daniel Lee (0430537003)',
+    desc: "A custom quoting system with floor plan drawing integration saves the business owner time to create both quotes and floor plans simultaneously on the same web application with guaranteed synchronised quote data that matches the drawings. This saves the headache of using 2 separate applications for quoting and floor plan design, and manually keeping them in sync for every client with ever-changing requirements.",
+    techStack: [
+        'API Server (Python/Django)',
+        'UI (Javascript/jQuery/Canvas)',
+        'Amazon Web Services'
+    ]
+};
+
+const employment2018 = {
+    endDate: 'a few months',
+    companyName: 'Spring Tech (now called CorePlan)',
+    url: 'https://springtech.io',
+    position: 'Senior Software Developer',
+    reference: 'Alex Goulios (Director)',
+    desc: "Design and build web applications with Python and JavaScript/Mithril to analyse and visualise data of mining operations stored in MSSQL database."
+};
+
+const employment2014 = {
+    endDate: '2016',
+    companyName: 'SignIQ (now called Last Yard)',
+    url: 'https://www.signiq.com',
+    position: 'Senior Software Developer',
+    reference: 'Chris Stoyles (CTO) (6230 2475)',
+    desc: "SignIQ is a leading Retail Ticketing company with customers across Australia and New Zealand. Senior Software Developer responsible for developing web applications in Python, Django, JavaScript and React."
+};
+
+const employment2012 = {
+    endDate: '2013',
+    companyName: 'Harmonic New Media',
+    url: 'http://harmonic.com.au',
+    position: 'Senior Software Developer',
+    reference: 'Craig Harman (Director) (9227 0003)',
+    desc: "A Perth based New Media company, responsible for building sports-tipping and e-commerce web applications used in large scale by large clients."
+};
+
 const resume = [
     ['h1', 'Robin Chew'],
     ['div', 'Software Engineer'],
@@ -12,4 +216,65 @@ const resume = [
     ['div', 'Australian Citizen'],
     ['h2', 'Experience'],
     ['div', expDesciption],
+    ['h2', 'Technology Stack'],
+    ['h3', 'Frontend'],
+    ['ul', frontendStacks],
+    ['h3', 'Backend'],
+    ['ul', backendStacks],
+    ['h3', 'Familiar Languages'],
+    ['ul', familiarStacks],
+    ['h3', 'Less familiar but can get work done'],
+    ['ul', lessFamiliar],
+    ['h2', 'General Skills'],
+    ['ul', skills],
+    ['h2', 'Contract Work'],
+    ['h3', '2023'],
+    ['ul', contract2023],
+    ['h3', '2022'],
+    ['ul', contract2022],
+    ['h3', '2021'],
+    ['h4', 'Qouted Work'],
+    ['ul', quotedWork2021],
+    ['h4', 'Part-Time Only Contract'],
+    ['ul', partTime2021],
+    ['h3', '2020'],
+    ['ul', contract2020],
+    ['h3', '2019'],
+    ['ul', contract2019],
+    ['h2', 'Projects developed under my OBSI business'],
+    ['div', obsiProjDesc],
+    ['h3', '2019 - ' + obsiProject2019.projName],
+    ['div', obsiProject2019.desc],
+    ['div', 'Technology Stack:'],
+    ['ul', obsiProject2019.techStack],
+    ['h3', '2017 - ' + obsiProject2017.projName],
+    ['div', obsiProject2017.desc],
+    ['div', 'Technology Stack:'],
+    ['ul', obsiProject2017.techStack],
+    ['h3', '2016 - ' + obsiProject2016.projName],
+    ['b', 'Owner: ' + obsiProject2016.owner],
+    ['div', obsiProject2016.desc],
+    ['div', 'Technology Stack:'],
+    ['ul', obsiProject2016.techStack],
+    ['h3', '2015 - ' + obsiProject2015.projName],
+    ['b', 'Owner: ' + obsiProject2015.owner],
+    ['div', obsiProject2015.desc],
+    ['div', 'Technology Stack:'],
+    ['ul', obsiProject2015.techStack],
+    ['h2', 'Past Employment'],
+    ['h3', '2018 ' + employment2018.endDate + ': ' + employment2018.companyName],
+    ['a',{href:employment2018.url},employment2018.url],
+    ['div', ['b', 'Position: ' + employment2018.position]],
+    ['b', 'Reference: ' + employment2018.reference],
+    ['div', employment2018.desc],
+    ['h3', '2014 - ' + employment2014.endDate + ': ' + employment2014.companyName],
+    ['a',{href:employment2014.url},employment2014.url],
+    ['div', ['b', 'Position: ' + employment2014.position]],
+    ['b', 'Reference: ' + employment2014.reference],
+    ['div', employment2014.desc],
+    ['h3', '2012 - ' + employment2012.endDate + ': ' + employment2012.companyName],
+    ['a',{href:employment2012.url},employment2012.url],
+    ['div', ['b', 'Position: ' + employment2012.position]],
+    ['b', 'Reference: ' + employment2012.reference],
+    ['div', employment2012.desc]
 ];
